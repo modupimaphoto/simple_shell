58409fce48511dcf5c5bfec6fb2ed9690603ba4f
Modupi <modupimaphoto@gmail.com>
2023-11-13 15:26:56 -0800

    Implement execute_command function - Introduce execute_command function in simple_shell.c, this function tokenizes the user input, checks for built-in commands (exit, cd), and forks a new process to execute external commands using fork_and_execute

---
+++ exe.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * execute_command - handles the execution of user commands
+ *
+ * @command: user command.
+ *
+ * Return: Nothing.
+ */
+void execute_command(char *command)
+{
+	/* Tokenize the command */
+	char *args[MAX_INPUT_SIZE];
+	int i = 0;
+	char *token = strtok(command, " \t\n");
+
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
+	if (i > 0)
+	{
+		/* Check for built-in commans */
+		if (strcmp(args[0], "exit") == 0)
+			exit(0);
+		else if (strcmp(args[0], "cd") == 0)
+		{
+			if (i > 1)
+			{
+				if (chdir(args[1]) != 0)
+					perror("cd");
+			}
+			else
+				fprintf(stderr, "cd: missing argument\n");
+		}
+		else
+		{
+			/* Fork and execute the command */
+			pid_t child_pid = fork_and_execute(command);
+
+			if (child_pid > 0)
+			{
+				/* Wats for child to finish */
+				waitpid(child_pid, NULL, 0);
+			}
+		}
+	}
+}
