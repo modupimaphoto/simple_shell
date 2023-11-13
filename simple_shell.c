79d2ffe380e8a3a66921ec456982418871c6276f
Modupi <modupimaphoto@gmail.com>
2023-11-13 15:30:17 -0800

    Implement main function - The main function orchestrates the shell's behavior and ensures a continuous loop for user interactions.

---
+++ simple_shell.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * main - reads user input.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t read;
+
+	while (1)
+	{
+		display_prompt();
+
+		read = getline(&input, &input_size, stdin);
+
+		if (read == -1)
+		{
+			/* Handle CTRL + D */
+			printf("\nExiting shell.\n");
+			free(input);
+			exit(0);
+		}
+
+		/* Remove newline character */
+		input[strspn(input, "\n")] = '\0';
+
+		/* Execute the command */
+		execute_command(input);
+	}
+
+	free(input);
+	return (0);
+}
