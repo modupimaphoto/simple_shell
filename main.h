a02fccdc6dab81b1864d1bc5f6b001ed6e1a22be
Modupi <modupimaphoto@gmail.com>
2023-11-13 15:24:14 -0800

    Header files - Required headers and Function Prototypes

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+
+void display_prompt(void);
+void execute_command(char *command);
+pid_t fork_and_execute(char *command);
+
+#endif
